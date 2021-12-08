7343e06b521e5553310b5e8ab64face4a11cd1f9
Zhuocai Li <zhuocaili@Zhuocais-MacBook-Air.local>
2021-12-08 00:16:51 -0800

    ?

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const path = require('path')
 const dbUrl = process.env.DB_URL
+
 // 'mongodb://localhost:27017/job-board'
 
 app.use(express.json())
